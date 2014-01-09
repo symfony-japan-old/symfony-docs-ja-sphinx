a771d2e2e0e908ba0e41e45a3bc589d7072a3ee8
hidenorigoto <hidenorigoto@gmail.com>
2014-01-09 17:09:04 +0900

    テンプレート修正

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 rm -rf symfony-docs-ja
 git clone git://github.com/symfony-japan/symfony-docs-ja.git
 
+cd symfony-docs-ja
+git clone git@github.com:symfony-japan/Create-Your-Framework-ja.git create-your-framework
+git clone git@github.com:symfony-japan/sf2-blog-tutorial-doc-ja.git sf2-blog-tutorial
+
+cd ..
+make html
+
 cd tempja
 cp -R ../symfony-docs-ja/* source/
 make html
