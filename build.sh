3009ed1c70835b20c4a8a8c69be7077aeeca6e96
hidenorigoto <hidenorigoto@gmail.com>
2014-01-09 17:18:14 +0900

    Drinkチュートリアルを含むようにbuild.shを修正した

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ git clone git://github.com/symfony-japan/symfony-docs-ja.git
 cd symfony-docs-ja
 git clone git@github.com:symfony-japan/Create-Your-Framework-ja.git create-your-framework
 git clone git@github.com:symfony-japan/sf2-blog-tutorial-doc-ja.git sf2-blog-tutorial
+git clone git@github.com:iteman/osc2011-nagoya-symfony2-tutorial.git
 
 cd ..
 make html
